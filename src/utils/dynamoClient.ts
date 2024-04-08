bb20830a25a06e302d159a9bef399e90d160e7ea
Michael Camacho <mikedev0431@gmail.com>
2024-04-08 01:21:38 -0700

    Change in environment variable

--- src/utils/dynamoClient.ts
+++ src/utils/dynamoClient.ts
@@ -5,7 +5,7 @@ const client = new DynamoDBClient({
   region: process.env.AWS_REGION ? process.env.AWS_REGION : "",
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY ? process.env.AWS_ACCESS_KEY : "",
-    secretAccessKey: process.env.AWS_SECRET_KEY ? process.env.AWS_SECRET_KEY : ""
+    secretAccessKey: process.env.AWS_PROD_SECRET_KEY ? process.env.AWS_PROD_SECRET_KEY : ""
   }
 });
 const dynamoClient = DynamoDBDocumentClient.from(client);

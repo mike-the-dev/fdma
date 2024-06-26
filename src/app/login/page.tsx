b530cbdc302b8507676670a66b30a278438ba58f
Michael Camacho <mikedev0431@gmail.com>
2024-06-26 01:54:39 -0700

    Added logo to login page.

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import styles from "../page.module.css";
 import LoginForm from "@/components/LoginForm";
+import JoymdLogo from "@/components/Logos/JoymdLogo";
+import { Spacer } from "@nextui-org/react";
 
 const Login: React.FC = async (): Promise<React.ReactElement> => {
   return (
     <div>
       <div className={styles.row}>
-        <div className={styles.column} style={{ display: "flex", justifyContent: "center" }}>
+        <div className={styles.column} style={{ display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column" }}>
+          <JoymdLogo />
+          <Spacer y={6} />
           <LoginForm />
         </div>
       </div>

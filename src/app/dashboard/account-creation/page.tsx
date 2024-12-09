02bb21e7e55656e9fb985f6d38330c6caebf4882
Michael Camacho <mikedev0431@gmail.com>
2024-12-09 10:58:40 -0800

    Added prop object to promise function.

--- src/app/dashboard/account-creation/page.tsx
+++ src/app/dashboard/account-creation/page.tsx
@@ -2,7 +2,11 @@ import CustomerCreation from "@/components/CustomerCreation";
 import React from "react";
 import styles from "../../page.module.css";
 
-const AccountCreation: React.FC = async (): Promise<React.ReactElement> => {
+interface AccountCreationProps {
+
+};
+
+const AccountCreation: React.FC<AccountCreationProps> = async (): Promise<React.ReactElement> => {
   return (
     <div>
       <div className={styles.row}>
